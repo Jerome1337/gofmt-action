3731f6a9c8355adf2ea07f4a9dee0202ed742676
Cyrus Javan <javan.cyrus@gmail.com>
2020-07-14 17:10:01 -0700

    Don't check for vendor files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd $GITHUB_WORKSPACE
 
-GOFMT_OUTPUT="$(gofmt -l "$1" 2>&1)"
+GOFMT_OUTPUT="$(gofmt -l `find $1 -name '*.go' | grep -v vendor` 2>&1)"
 
 if [ -n "$GOFMT_OUTPUT" ]; then
   echo "All the following files are not correctly formatted"
